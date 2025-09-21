7fd14c0107deae7e9e9ace7d3d6e742acc2e314e
hyein89 <bihontai@gmail.com>
2025-09-21 21:01:48 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-
+import "../styles/404.css"; // tambahkan ini
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
