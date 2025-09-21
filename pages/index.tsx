394f02d968add7688fae78ca9fb2857e680fa644
hyein89 <bihontai@gmail.com>
2025-09-21 21:21:55 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
         <title>Coming Soon</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="/home.css" />
+        <link rel="icon" href="/xxlove.png">
       </Head>
 
       <div className="container text-center">
