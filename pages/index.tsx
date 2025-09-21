b72b0585c384f41a1d6a2d272a899fdd0ff7464d
hyein89 <bihontai@gmail.com>
2025-09-21 20:38:09 +0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Halaman Utama</title>
+      </Head>
+      <main className="flex h-screen items-center justify-center">
+        <h1 className="text-2xl font-bold">Halaman Utama (Kosong dulu)</h1>
+      </main>
+    </>
+  );
+}
